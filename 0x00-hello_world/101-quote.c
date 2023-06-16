a23a0502005dfe7b23361288fb5e06170fa6c43b
mhmd9696 <ayad96mhmd@gmail.com>
2023-06-15 19:43:28 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program that prints a quote with write funcition
+ *
+ * Return: 1 (not success)
+*/
+
+int main(void)
+{
+        char quto[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1,quo,59);
+        return (1);
+}
