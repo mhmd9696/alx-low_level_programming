ab9875fbfaefa27f19146d90034c4de67cf16f00
mhmd9696 <ayad96mhmd@gmail.com>
2023-06-21 14:06:04 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,16 @@
  * main - function to print
  *
  * Return: 0
- */
+*/
+
 int main(void)
 {
-	char str[] = {_putchar};
+	char str[] = "_putchar";
 	int ch;
 
-	for (c = 0; ch < 8; c++)
-	{
+	for (ch = 0; ch < 8; c++)
 		_putchar(str[ch]);
-	}
-_putchar('\n');
-return (0);
+	_putchar('\n');
+
+	return (0);
 }
